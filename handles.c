feb677720f73e41f3b821c7d6c3e6f2aa3b1f9ee
RaymondEdibo <raymondedibo1@gmail.com>
2023-06-27 06:56:22 -0700

    work

--- handles.c
+++ handles.c
@@ -24,11 +24,53 @@ int handle_c(va_list args)
 int handle_s(va_list args)
 {
 	char *s = va_arg(args, char*);
+	int l = 0;
 
 	if (s == NULL)
 		s = "(null)";
-	write(1, s, strlen(s));
-	return (strlen(s));
+	while (s[l])
+		l++;
+	write(1, s, l);
+	return (l);
+}
+/**
+ *handle_d - check the code
+ *@args:
+ *
+ *Return: length
+ */
+int handle_d(va_list args)
+{
+	int num = va_arg(args, int);
+	int l = 0;
+
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+		l++;
+	}
+
+	l += print_number(num);
+
+	return (l);
+}
+/**
+ *print_number - check the code
+ *@num:
+ *
+ *Return: length
+ */
+int print_number(int num)
+{
+	char digit;
+
+	if (num / 10 != 0)
+		print_number(num / 10);
+	digit = (num % 10) + '0';
+	write(1, &digit, 1);
+
+	return (1);
 }
 /**
  *handle_percent - check the code
--- main.h
+++ main.h
@@ -6,5 +6,6 @@ int _printf(const char *format, ...);
 int handle_c(va_list args);
 int handle_s(va_list args);
 int handle_percent();
+int print_number(int num);
 
 #endif
